c8cfd4ef0e425627a3a74917062fb2a2cbd0c637
minkyung_kwak <ellen10min@gmail.com>
2023-07-05 01:48:44 +0900

    삭제 기능 구현중

--- 2주차/stopwatch/app.js
+++ 2주차/stopwatch/app.js
@@ -1,76 +1,89 @@
-const timeElement = document.getElementById("time-number");
-const startButton = document.getElementById("start-btn");
-const stopButton = document.getElementById("stop-btn");
-const resetButton = document.getElementById("reset-btn");
+document.addEventListener("DOMContentLoaded", () => {
+  const timeElement = document.getElementById("time-number");
+  const startButton = document.getElementById("start-btn");
+  const stopButton = document.getElementById("stop-btn");
+  const resetButton = document.getElementById("reset-btn");
 
-let startTime;
-let timer;
+  let startTime;
+  let timer;
 
-function formatTime(time) {
-  let minutes = Math.floor(time / 60000);
-  let seconds = Math.floor((time % 60000) / 1000);
-  return (
-    (minutes < 10 ? "0" : "") +
-    minutes +
-    ":" +
-    (seconds < 10 ? "0" : "") +
-    seconds
-  );
-}
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60000);
+    let seconds = Math.floor((time % 60000) / 1000);
+    return (
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds
+    );
+  };
 
-function startTimer() {
-  startTime = Date.now();
-  timer = setInterval(updateTimer, 1000);
-  console.log(startTime);
-}
+  const startTimer = () => {
+    startTime = Date.now();
+    timer = setInterval(updateTimer, 1000);
+    console.log(startTime);
+  };
 
-function updateTimer() {
-  let presentTime = Date.now();
-  let elapsedTime = presentTime - startTime;
-  timeElement.textContent = formatTime(elapsedTime);
-}
+  const updateTimer = () => {
+    let presentTime = Date.now();
+    let elapsedTime = presentTime - startTime;
+    timeElement.textContent = formatTime(elapsedTime);
+  };
 
-function stopTimer() {
-  const currentTime = formatTime(Date.now() - startTime);
-  createRecord(currentTime);
-}
+  const stopTimer = () => {
+    const currentTime = formatTime(Date.now() - startTime);
+    createRecord(currentTime);
+  };
 
-function createRecord(time) {
+  const createRecord = (time) => {
+    const recordContainer = document.querySelector(".record-box");
+
+    const recordElement = document.createElement("div");
+    recordElement.className = "record-time";
+
+    const imageElement = document.createElement("img");
+    imageElement.src = "rec.png";
+    imageElement.className = "check-btn";
+    recordElement.appendChild(imageElement);
+
+    const timeElement = document.createElement("span");
+    timeElement.textContent = time;
+    recordElement.appendChild(timeElement);
+
+    recordContainer.appendChild(recordElement);
+  };
+
+  const resetTimer = () => {
+    timeElement.textContent = "00:00";
+    clearInterval(timer);
+  };
+
+  startButton.addEventListener("click", startTimer);
+  stopButton.addEventListener("click", stopTimer);
+  resetButton.addEventListener("click", resetTimer);
+
   const recordContainer = document.querySelector(".record-box");
 
-  const recordElement = document.createElement("div");
-  recordElement.className = "record-time";
+  recordContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("check-btn")) {
+      const checkBtn = event.target;
+      if (checkBtn.classList.contains("changed")) {
+        checkBtn.src = "rec.png";
+        checkBtn.classList.remove("changed");
+      } else {
+        checkBtn.src = "button.png";
+        checkBtn.classList.add("changed");
+      }
+    }
+  });
 
-  const imageElement = document.createElement("img");
-  imageElement.src = "rec.png";
-  imageElement.className = "check-btn";
-  recordElement.appendChild(imageElement);
-
-  const timeElement = document.createElement("span");
-  timeElement.textContent = time;
-  recordElement.appendChild(timeElement);
-
-  recordContainer.appendChild(recordElement);
-}
-
-function resetTimer() {
-  timeElement.textContent = "00:00";
-  clearInterval(timer);
-}
-
-startButton.addEventListener("click", startTimer);
-stopButton.addEventListener("click", stopTimer);
-resetButton.addEventListener("click", resetTimer);
-
-const checkBtn = document.querySelector(".check-btn");
-let isChanged = false;
-
-checkBtn.addEventListener("click", () => {
-  if (isChanged) {
-    checkBtn.src = "rec.png";
-    isChanged = false;
-  } else {
-    checkBtn.src = "button.png";
-    isChanged = true;
-  }
+  const trashRemove = document.querySelector(".trash-remove");
+  trashRemove.addEventListener("click", (event) => {
+    const recordTime = event.target.closest(".record-time");
+    const checkBtn = recordTime.querySelector(".check-btn");
+    if (checkBtn && checkBtn.src.includes("button.png")) {
+      recordContainer.removeChild(recordTime);
+    }
+  });
 });

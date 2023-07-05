f910c3af679aec94215bbe1ce1f7385dc82d966a
minkyung_kwak <ellen10min@gmail.com>
2023-07-05 20:38:12 +0900

    Add: 1-6강 display items

--- 2주차/Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ 2주차/Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -1,15 +1,34 @@
-console.log("hello");
 // Fetch the items from the JSON file
 function loadItems() {
-  return fetch("data/data.json")
+  return fetch(`data/data.json`)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => json.items);
 }
+
+//Update the list with the given items
+
+const displayItems = (items) => {
+  const container = document.querySelector(".items");
+  const html = items.map((item) => createHTMLString(item)).join("");
+  console.log(html);
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+};
+
+//Create HTML list item from the given data item
+
+const createHTMLString = (item) => {
+  return `
+  <li class="item">
+    <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+    <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+};
+
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items);
   })
   .catch(console.log);

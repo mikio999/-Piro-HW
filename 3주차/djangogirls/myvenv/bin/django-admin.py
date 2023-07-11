cf65e87af90bfe93c4b0a6fdd512ba76c4062f68
minkyung_kwak <ellen10min@gmail.com>
2023-07-12 01:31:55 +1000

    Add: HTML 시작하기

---
+++ 3주차/djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/minkyung_kwak/Desktop/Piro_github/piro19_hw/3주차/djangogirls/myvenv/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

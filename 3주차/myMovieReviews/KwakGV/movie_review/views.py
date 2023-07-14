b09a7180b2611efcfc5f8aec2fe046aff18acbac
minkyung_kwak <ellen10min@gmail.com>
2023-07-14 13:47:53 +0900

    Add: 다시 시작

--- 3주차/myMovieReviews/KwakGV/movie_review/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import myText
-
-# Create your views here.
-def movie_list(request) :
-    texts = myText.objects.filter()
-    return render(request, 'movie_review/movie_list.html', {'texts':texts})
